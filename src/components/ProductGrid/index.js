28d868f010d79328c5ea15c76e5a4056c26ae309
Maciej Kurek <maciejk77@gmail.com>
2019-09-07 16:23:19 +0100

    onClick handler, minor

--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import data from '../../data/productData.json';
 
 const ProductGrid = () => {
+  const handleClick = e => {
+    console.log(e.target.value);
+  };
+
   return data.products.map(product => {
     return (
-      <button key={product.productCode}>
+      <button
+        onClick={handleClick}
+        key={product.productCode}
+        value={product.productCode}
+      >
         {product.description} £{(product.price / 100).toFixed(2)}
       </button>
     );
