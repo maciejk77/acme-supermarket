4c2a8e2544d822f85d62513dec3dbb50a6b67fea
Maciej Kurek <maciejk77@gmail.com>
2019-09-08 12:38:12 +0100

    ShoppingList component udpated with useContext hook

--- src/components/ShoppingList/index.js
+++ src/components/ShoppingList/index.js
@@ -3,7 +3,22 @@ import { BasketContext } from '../BasketContext';
 
 const ShoppingList = () => {
   const { basket } = useContext(BasketContext);
-  return basket.map((basketItem, idx) => <div key={idx}>{basketItem}</div>);
+  return basket.map((basketItem, idx) => (
+    <div style={styles.shoppingListStyle} key={idx}>
+      {basketItem}
+    </div>
+  ));
+};
+
+// quick fix, temp styles to be replaced with Sass or styled components
+const styles = {
+  shoppingListStyle: {
+    border: '1px solid red',
+    padding: '1rem',
+    fontSize: '1rem',
+    color: 'red',
+    margin: '.5rem'
+  }
 };
 
 export default ShoppingList;
