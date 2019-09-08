0858abe7a3fde265c40ce70ce606c78dd34355e1
Maciej Kurek <maciejk77@gmail.com>
2019-09-08 11:39:03 +0100

    adding basket items in ProductGrid

--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -1,21 +1,75 @@
-import React from 'react';
-import data from '../../data/productData.json';
+import React, { useState } from 'react';
+import { products } from '../../data/productData.json';
 
 const ProductGrid = () => {
+  const [basket, addToBasket] = useState([]);
   const handleClick = e => {
-    console.log(e.target.value);
+    addToBasket([...basket, e.target.value]);
   };
 
-  return data.products.map(product => {
+  //console.log(basket); // TO BE DELETED
+  return products.map(({ productCode, description, price }) => {
     return (
       <button
+        style={styles.buttonStyle}
+        key={productCode}
+        value={productCode}
         onClick={handleClick}
-        key={product.productCode}
-        value={product.productCode}
       >
-        {product.description} £{(product.price / 100).toFixed(2)}
+        {description} £{(price / 100).toFixed(2)}
       </button>
     );
   });
 };
+
+// quick fix, temp styles to be replaced with Sass or styled components
+const styles = {
+  buttonStyle: {
+    border: 'none',
+    padding: '1rem',
+    fontSize: '1rem',
+    backgroundColor: 'red',
+    color: 'white',
+    margin: '.5rem',
+    outline: 'none'
+  }
+};
+
 export default ProductGrid;
+
+// import React, { useReducer, useState } from 'react';
+// import data from '../../data/productData.json';
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'ADD_TO_BASKET':
+//       return { basketItems: [...state.basketItems, { foo: action.payload }] };
+//     default:
+//       return state;
+//   }
+//   // console.log(e.target.value);
+// };
+
+// const ProductGrid = () => {
+//   const [{ basketItems }, dispatch] = useReducer(reducer, { basketItems: [] });
+//   const [selectedItem, setSelectedItem] = useState();
+//   console.log(basketItems);
+//   return data.products.map(product => {
+//     return (
+//       <div>
+//         <button
+//           key={product.productCode}
+//           value={product.productCode}
+//           onClick={e => {
+//             dispatch({ type: 'ADD_TO_BASKET', payload: selectedItem });
+//             setSelectedItem(e.target.value);
+//           }}
+//         >
+//           {product.description} £{(product.price / 100).toFixed(2)}
+//         </button>
+//         <pre>{JSON.stringify(basketItems, null, 2)}</pre>
+//       </div>
+//     );
+//   });
+// };
+// export default ProductGrid;
